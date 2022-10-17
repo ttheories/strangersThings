854b5e6210d6afffc05c74c30c5f4a65ca0888a1
Therese <ttheories@gmail.com>
2022-10-17 14:29:27 -0700

    started nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
 import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navigation from "./components/Navigation";
 import Register from "./components/Register";
 import { fetchMe } from "./api/auth";
-import "./App.css";
-
 
 function App() {
-  const [token, setToken] =useState(localStorage.getItem("token"));
+  const [token, setToken] = useState(localStorage.getItem("token"));
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    async function getMe(){
-    console.log("token before fetch");
-    const result = await fetchMe(token);
-    console.log(result);
-    setUser(result.data);
-  }
-  if (token){
-    getMe();
+    async function getMe() {
+      console.log("token before fetch");
+      const result = await fetchMe(token);
+      console.log(result);
+      setUser(result.data);
+    }
+    if (token) {
+      getMe();
     }
     console.log("in the useEffect");
   }, [token]);
 
   return (
     <div className="App">
-      <h4>
-        {user?.username}
-      </h4>
-      <Register setToken={setToken}/>
+      <Navigation />
+      <Routes>
+        <Route path="/Register" element={<Register setToken={setToken} />} />
+      </Routes>
+      <h4>{user?.username}</h4>
     </div>
   );
-
-  
-  }
-
+}
 
 export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <div>
+
+      <h2>Stranger's Things</h2>
+      <Link to="/">HOME</Link>
+      <Link to="/Register">Register</Link>
+      <Link to="/">Login/Logout</Link>
+      <Link to="/ ">Profile</Link>
+    </div>
+  );
+}
+
+export default Navigation;
